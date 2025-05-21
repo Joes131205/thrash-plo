bf4d7f517dc7fff3572fe03ee7b4f4197951e4c4
Sheren Aura Vi <sherenaura21@gmail.com>
2025-05-21 19:22:51 +0700

    feat: add section our mission

--- client/src/assets/images/index.tsx
+++ client/src/assets/images/index.tsx
@@ -2,5 +2,6 @@ import ImgLogo from "../../assets/images/img-logo.png";
 import ImgLandingLeft from "../../assets/images/img-landing-left.png";
 import ImgLandingRight from "../../assets/images/img-landing-right.png";
 import ImgLanding from "../../assets/images/img-landing.png";
+import ImgMisiKita from "../../assets/images/img-mission.png";
 
-export { ImgLogo, ImgLandingLeft, ImgLandingRight, ImgLanding };
+export { ImgLogo, ImgLandingLeft, ImgLandingRight, ImgLanding, ImgMisiKita };
--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImgLanding, ImgLogo } from "@/assets/images";
+import { ImgLanding, ImgLogo, ImgMisiKita } from "@/assets/images";
 import styles from "./LandingPage.module.css";
 import ButtonMain from "@/components/atomics/buttonMain/buttonMain";
 
@@ -45,6 +45,21 @@ export default function LandingPage() {
 
       <div className={styles.separator}></div>
       {/* END BANNER */}
+
+      {/* MISI KITA */}
+      <div className={styles.misiKita}>
+        <div className={styles.textLeft}>
+          <h2 className={styles.titleMisi}>Misi Kita</h2>
+          <p className={styles.descMisi}>
+            TrashPlo hadir dengan misi untuk membangun ekosistem kolaboratif dalam pengelolaan sampah yang efektif, transparan, dan berkelanjutan. Kami berfokus pada pemberdayaan masyarakat untuk aktif melaporkan titik-titik sampah liar,
+            mendukung komunitas lingkungan dalam aksi bersih, serta mempercepat koordinasi dengan instansi pemerintah untuk penanganan sampah yang lebih responsif.{" "}
+          </p>
+        </div>
+        <div className={styles.imgRight}>
+          <img src={ImgMisiKita} alt="Misi Kita" className={styles.imgRight} />
+        </div>
+      </div>
+      {/* END MISI KITA */}
     </div>
   );
 }
